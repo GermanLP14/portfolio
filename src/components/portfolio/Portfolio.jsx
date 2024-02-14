35c6a7af7e9e0e9f179f544ddfc2d5ba9a31fe39
German <germanlauritopass14@gmail.com>
2024-02-14 13:38:56 -0300

    Curriculum terminado sin responsive

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,42 +7,42 @@ const items = [
   {
     id: 1,
     title: "Censo 2023",
-    img: "./public/Censo 2023.png",
+    img: "Censo 2023.png",
     desc: "Frontend development that simulates Uruguay's census. I used technologies such as HTML, CSS and React to create an intuitive and accessible user interface that made it easy for citizens to participate in the census.",
     url: "https://censouruguay.netlify.app"
   },
   {
     id: 2,
     title: "Api Marvel",
-    img: "./public/Api marvel.png",
+    img: "Api marvel.png",
     desc: "In this frontend project, I developed a web application that interacts with the Marvel API to display a gallery of various comic book characters. Used HTML to structure the page, CSS for layout and styling, and JavaScript to make API requests and dynamically display the characters",
     url: "https://germanlp14.github.io/Api-Marvel/index.html"
   },
   {
     id: 3,
     title: "A Day To Remember",
-    img: "/public/A day to remember.png",
+    img: "A day to remember.png",
     desc: "In this project, I designed a website inspired by the band A Day to Remember using HTML and CSS. The page features a professional and attractive design that reflects the aesthetics and energy of the band. I incorporated striking visual elements, such as images(art) of the band and albums, along with a dynamic color palette that highlights the band's identity.",
     url: "https://germanlp14.github.io/A-Day-To-Remember/index.html"
   },
   {
     id: 4,
     title: "Bienes Raices",
-    img: "/public/bienesRaices.png",
+    img: "bienesRaices.png",
     desc: "This page is one of my first projects working with HTML and CSS. It represents a real estate page divided into sections providing information through a simple but modern design.",
     url: "https://wonderful-bartik-24e60a.netlify.app/"
   },
   {
     id: 5,
     title: "Blog Café",
-    img: "/public/cafe.png",
+    img: "cafe.png",
     desc: "Using HTML and CSS I carried out this work that represents a page/blog that is about coffee and its different uses. Simple and minimalist but attractive design that, combined with a color palette, transmits the essence of coffee.",
     url: "https://heuristic-albattani-42658b.netlify.app/"
   },
   {
     id: 6,
     title: "FrontEnd Store",
-    img: "/public/store.png",
+    img: "store.png",
     desc: "Simple project which represents a thematic store for developers. Focusing on a simple and friendly interface, I used striking colors and clear images that can attract new users.",
     url: "https://eager-payne-908c31.netlify.app/"
   },
